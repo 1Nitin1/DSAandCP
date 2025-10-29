da28403b7c06899727ed028c4b7da1830bd0e101
Nitin Baranwal <nitinkumarbaranawal@gmail.com>
2025-10-29 23:56:39 +0530

    Leetcode

--- src/Leetcode/Easy/LCSmallestNumberWithAllSetBits.java
+++ src/Leetcode/Easy/LCSmallestNumberWithAllSetBits.java
@@ -1,2 +1,8 @@
-package Leetcode.Easy;public class LCSmallestNumberWithAllSetBits {
+package Leetcode.Easy;
+
+public class LCSmallestNumberWithAllSetBits {
+    public int smallestNumber(int n) {
+        int a = (int)((Math.log(n)/Math.log(2))+1);
+        return (int)Math.pow(2,a)-1;
+    }
 }
