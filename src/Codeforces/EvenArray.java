a02e10f2257d23284588996827b77145b08c5067
Nitin Baranwal <nitinkumarbaranawal@gmail.com>
2025-11-10 21:08:41 +0530

    Codeforces

--- src/Codeforces/EvenArray.java
+++ src/Codeforces/EvenArray.java
@@ -1,2 +1,37 @@
-package Codeforces;public class EvenArray {
+package Codeforces;
+
+import java.util.*;
+
+public class EvenArray {
+
+    public static int minMovesToMakeGood(int[] a) {
+        int badEven = 0; // even index has odd value
+        int badOdd = 0;  // odd index has even value
+
+        for (int i = 0; i < a.length; i++) {
+            if (i % 2 != a[i] % 2) {
+                if (i % 2 == 0) badEven++;
+                else badOdd++;
+            }
+        }
+
+        if (badEven != badOdd) return -1;
+        return badEven;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] a = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+
+            System.out.println(minMovesToMakeGood(a));
+        }
+    }
 }
--- src/Codeforces/OnlyOneDigit.java
+++ src/Codeforces/OnlyOneDigit.java
@@ -1,2 +1,27 @@
-package Codeforces;public class OnlyOneDigit {
+package Codeforces;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+public class OnlyOneDigit {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+        int t=Integer.parseInt(br.readLine());
+        while(t-->0){
+            int n=Integer.parseInt(br.readLine());
+            int ans=9;
+            while(n>0){
+                int k=n%10;
+                if(k<ans){
+                    ans=k;
+                }
+                n/=10;
+            }
+            out.println(ans);
+        }
+        out.flush();
+    }
 }
